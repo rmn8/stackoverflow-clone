a6d820b1ef70b5e4785cef57b4c94e35a96492c6
Ram M Nambiar <ramohannambiar@gmail.com>
2023-12-31 09:40:11 -0500

    See if process.env is read

--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -92,6 +92,8 @@ const Auth = () => {
 
                     <label htmlFor='email'>
                         <h4>Email</h4>
+                        <h1>asas</h1> 
+
                         <input type="email" name="email" id="email" onChange={(e) => { setEmail(e.target.value) }} />
                     </label>
                     <label htmlFor='password'>
